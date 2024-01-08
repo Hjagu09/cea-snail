1451bd17fb685e8ecbcae2d42670adad0ac68b44
Hjalmar Östling <hjalmarostling@gmail.com>
2024-01-08 11:05:14 +0100

    new std lib functions

--- rc/c++/lib/stdlib.cpp
+++ rc/c++/lib/stdlib.cpp
@@ -6,6 +6,26 @@ void EXPOSE(print)(std::string x)
 	std::cout << x << std::endl;
 	#endif
 }
+std::string EXPOSE(input)(std::string promt){
+    #if defined(__EMSCRIPTEN__)
+    set_promt(&promt[0], promt.length());
+    input_setup();
+    while (1)
+    {
+    	if (!check_input())
+    	{
+    		return std::string(input());
+    	}
+    	emscripten_sleep(100);
+    }
+    #else
+    std::cout << promt;
+    std::string x;
+    std::cin >> x;
+    std::cout << std::endl;
+    return x;
+    #endif
+}
 
 void EXPOSE(for)(int low, int high, std::function<void(int)> action)
 {
